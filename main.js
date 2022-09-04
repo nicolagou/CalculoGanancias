36d7e4c91016ef0aa766a16b3b7bdf2fa855c89e
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-09-04 20:07:45 -0300

    Agregado el array con ingreso manual y una funcion de orden superior

--- main.js
+++ main.js
@@ -7,58 +7,82 @@ let valorIg = 0;
 let valorIva = 0;
 let valorIvaCompra = 0;
 
+const datosFacturas = []
 
-while (true) {
-    precioFactura = parseFloat(prompt("Ingrese el precio de su servicio que va a cobrar en su Factura A (numero mayor a cero)"));
-    if (!isNaN(precioFactura) && precioFactura != null && precioFactura != "") {
-        //es numero
-        console.log("Precio: " + precioFactura)
-        precioCosto = parseFloat(prompt("Ingrese los gastos facturados que le genero el servicio"));
-        if (!isNaN(precioCosto) && precioCosto != null && precioCosto != "") {
+// crear un objeto (datos de factura) para el array (lista de facturas)
+function crearDatosFactura(precioFactura, precioCosto, valorIibb, valorIg, valorIva, valorIvaCompra) {
+    this.precioFactura = precioFactura;
+    this.precioCosto = precioCosto;
+    this.valorIibb = valorIibb;
+    this.valorIg = valorIg;
+    this.valorIva = valorIva;
+    this.valorIvaCompra = valorIvaCompra;
+}
+
+function cargarFactura(arr, valor) {
+    arr.push(valor);
+}
+
+// crear objetos datos factura
+for (let index = 0; index < 2; index++) {
+    while (true) {
+        precioFactura = parseFloat(prompt("Trabajo " + (1 + index) + ": " + "Ingrese el precio de su servicio que va a cobrar en su Factura A (numero mayor a cero)"));
+        if (!isNaN(precioFactura) && precioFactura != null && precioFactura != "") {
             //es numero
-            console.log("Gastos: " + precioCosto)
-            valorIibb = parseFloat(prompt("Ingrese el % de IIBB que se le retiene segun su actividad (e.g.3.5)"));
-            if (!isNaN(valorIibb) && valorIibb != null && valorIibb != "") {
+            // console.log("Precio: " + precioFactura)
+            precioCosto = parseFloat(prompt("Trabajo " + (1 + index) + ": " + "Ingrese los gastos facturados que le genero el servicio"));
+            if (!isNaN(precioCosto) && precioCosto != null && precioCosto != "") {
                 //es numero
-                console.log("Valor IIBB: " + valorIibb + "%")
-                valorIg = parseFloat(prompt(("Ingrese el % de Impuesto a las Ganancias que se le retiene segun su actividad (e.g.30)")));
-                if (!isNaN(valorIg) && valorIg != null && valorIg != "") {
+                // console.log("Gastos: " + precioCosto)
+                valorIibb = parseFloat(prompt("Trabajo " + (1 + index) + ": " + "Ingrese el % de IIBB que se le retiene segun su actividad (e.g.3.5)"));
+                if (!isNaN(valorIibb) && valorIibb != null && valorIibb != "") {
                     //es numero
-                    console.log("Valor IG: " + valorIg + "%")
-                    valorIva = parseFloat(prompt("Ingrese el % de IVA que se le retiene segun su actividad (e.g.21)"));
-                    if (!isNaN(valorIva) && valorIva != null && valorIva != "") {
+                    // console.log("Valor IIBB: " + valorIibb + "%")
+                    valorIg = parseFloat(prompt(("Trabajo " + (1 + index) + ": " + "Ingrese el % de Impuesto a las Ganancias que se le retiene segun su actividad (e.g.30)")));
+                    if (!isNaN(valorIg) && valorIg != null && valorIg != "") {
                         //es numero
-                        console.log("Valor IVA: " + valorIva + "%")
-                        valorIvaCompra = parseFloat(prompt("Ingrese el % de IVA que se aplico a la factura de los gastos realizados (e.g.21)"));
-                        if (!isNaN(valorIvaCompra) && valorIvaCompra != null && valorIvaCompra != "") {
+                        // console.log("Valor IG: " + valorIg + "%")
+                        valorIva = parseFloat(prompt("Trabajo " + (1 + index) + ": " + "Ingrese el % de IVA que se le retiene segun su actividad (e.g.21)"));
+                        if (!isNaN(valorIva) && valorIva != null && valorIva != "") {
                             //es numero
-                            console.log("Valor IVA Compra: " + valorIvaCompra + "%")
-                            break;
+                            // console.log("Valor IVA: " + valorIva + "%")
+                            valorIvaCompra = parseFloat(prompt("Trabajo " + (1 + index) + ": " + "Ingrese el % de IVA que se aplico a la factura de los gastos realizados (e.g.21)"));
+                            if (!isNaN(valorIvaCompra) && valorIvaCompra != null && valorIvaCompra != "") {
+                                //es numero
+                                // console.log("Valor IVA Compra: " + valorIvaCompra + "%")
+                                break;
+                            } else {
+                                console.log("Ingrese un numero valido de IVA Compra");
+                                continue;
+                            };
                         } else {
-                            console.log("Ingrese un numero valido de IVA Compra");
+                            console.log("Ingrese un numero valido de IVA");
                             continue;
                         };
                     } else {
-                        console.log("Ingrese un numero valido de IVA");
+                        console.log("Ingrese un numero valido de IG");
                         continue;
                     };
                 } else {
-                    console.log("Ingrese un numero valido de IG");
+                    console.log("Ingrese un numero valido de IIBB");
                     continue;
                 };
             } else {
-                console.log("Ingrese un numero valido de IIBB");
+                console.log("Ingrese un numero valido de Gastos");
                 continue;
             };
         } else {
-            console.log("Ingrese un numero valido de Gastos");
+            console.log("Ingrese un numero valido del monto de la factura");
             continue;
-        };
-    } else {
-        console.log("Ingrese un numero valido del monto de la factura");
-        continue;
+        }
     }
+    const nuevaFactura = new crearDatosFactura(precioFactura, precioCosto, valorIibb, valorIg, valorIva, valorIvaCompra)
+    cargarFactura(datosFacturas, nuevaFactura)
+
 }
+console.log(datosFacturas);
+
+
 
 
 //Funcion Precio sin IVA
@@ -106,3 +130,19 @@ function informarResultado(factura, gastos, facturaSinIva, gastosSinIva, ig, iib
     alert("Precio cobrado: " + factura + "\nGastos " + gastos + "\nPrecio cobrado s/IVA: " + facturaSinIva + "\nGastos s/IVA: " + gastosSinIva + "\nIG: " + ig + "\nIIBB: " + iibb + "\nGanancias: " + totalGanancias)
 }
 let Resumen = informarResultado(precioFactura, precioCosto, facturaSinIva, gastosSinIva, resultadoIg, resultadoIibb, Ganancia)
+
+
+
+//Funcion de orden superior - Filtrado:
+function filtrarFactura(arr, filtro) {
+    const filtrado = arr.filter((el) => {
+        return el.precioFactura > filtro
+    })
+    return filtrado
+}
+
+ingresarFiltro = prompt ("Mostrar datos de las facturas con precio mayor a: ");
+
+const resultadoFiltro = filtrarFactura(datosFacturas,ingresarFiltro);
+
+console.log(resultadoFiltro);
