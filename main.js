343d1f6e63c71ac6bafa5179f150381750337b9f
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-09-26 16:10:48 -0300

    Agregado Toastify

--- main.js
+++ main.js
@@ -81,6 +81,21 @@ btnCargar.addEventListener("click", () => {
     tbody.innerHTML = "";
     crearTablaHtml(datosFacturas);
     localStorage.setItem("datosFacturas", JSON.stringify(datosFacturas));
+
+    //agregado botton que confirma la carga de los datos
+    Toastify({
+        text: "Factura Cargada",
+        duration: 3000,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
 });
 
 btnVer.addEventListener("click", () => {
